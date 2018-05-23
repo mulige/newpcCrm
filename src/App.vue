ac61c8c54d5d24840d21697e1ed9452fdbcb668a
wjf296369555 <sxdxwjf@126.com>
2018-05-23 10:57:21 +0800

    基础文件创建

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <style>
+@import'./styles/app.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/store/actions.js
@@ -0,0 +1,10 @@
+import {
+  fetchUser
+} from './api'
+import axios from 'axios'
+export default {
+  // ensure data for rendering given list type
+  
+  
+  
+}
